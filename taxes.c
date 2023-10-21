d29fd6ff1f993cbfe6f80315286d1551bb8dab15
hifza <hifzanazir456@gmail.com>
2023-10-21 15:15:16 +0500

    code for next condition,by using tables...

--- taxes.c
+++ taxes.c
@@ -45,6 +45,20 @@ int main(int argc, char **argv) {
     totalTax = tax * (agi-0);
     tax = tax - childCredit;
   }
+   else if(agi>=19901 && agi<=81050)
+  {
+    if(agi>=20000 && agi<=120000 && numChildren==0)
+    {
+      childCredit = 2002.00;
+    }
+    else 
+    {
+      childCredit = numChildren*2000;
+    }
+    tax = agi * (12.0/100);
+    totalTax = tax * (agi-19900);
+    tax = tax - childCredit;
+  }
   
 
   printf("AGI:          $%10.2f\n", agi);

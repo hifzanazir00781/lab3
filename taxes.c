51351eeefd61e9374c1e318c5e2a46c51fd376ac
hifza <hifzanazir456@gmail.com>
2023-10-21 15:11:57 +0500

    nested if else statement ,code of 1st condition...

--- taxes.c
+++ taxes.c
@@ -30,45 +30,21 @@ int main(int argc, char **argv) {
   }
 
   //TODO: compute the tax, child credit, and total tax here
+  // there i will you the nested if else statement by taking the data from tables
   if(agi>=0 && agi<=19900)
   {
-    tax = agi * 0.10;
-    printf("%f",tax);
+    if(agi>=4000 && agi<=20000 && numChildren==1)
+    {
+      childCredit = 0;
+    }
+    else 
+    {
+      childCredit = numChildren*2000;
+    }
+    tax = agi * (10.0/100);
+    totalTax = tax * (agi-0);
+    tax = tax - childCredit;
   }
-  else if(agi>=19901 && agi<=81050)
-  {
-    tax=1990+(agi-19900) * 0.12;
-    printf("%f",tax);
-  }
- else if (agi>=81051 && agi <= 172750)
- {
-  
-    tax=9328+(agi-81050 ) * 0.22;
-    printf("%f",tax);
-  }
-  else if (agi>=172751 && agi <=329850 )
- {
-  
-    tax=29505+(agi-172750 ) * 0.24;
-    printf("%f",tax);
-  }
-  else if (agi>=329851 && agi <= 418850)
- {
-  
-    tax=67206+(agi-329850 ) * 0.32;
-    printf("%f",tax);
-  }
-  else if (agi>=418851 && agi <= 628300)
- {
-  
-    tax=95686+(agi-418850 ) * 0.35;
-    printf("%f",tax);
-  }
- }
-  
-  childCredit = numChildren * 1000;
-  totalTax = childCredit - tax;
-
   
 
   printf("AGI:          $%10.2f\n", agi);
